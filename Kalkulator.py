b927fc9b138ac0c83b8d1672e4efe4d8f3e0a361
hamdan0114 <147163660+hamdan0114@users.noreply.github.com>
2024-03-23 14:09:24 +0700

    Add files via upload

---
+++ Kalkulator.py
@@ -0,0 +1,4 @@
+celcius = input ("masukan celcius :")
+Format = 9 * int(celcius) / 5 + 32
+
+print (Format, "fahreinheit")
---
+++ Nama.py
@@ -0,0 +1,4 @@
+Nama = input ('masukan nama :')
+Mata_kuliah = input ('masukan mata kuliah :')
+
+print ("hallo selamat datang", Nama , ", selamat belajar", Mata_kuliah, "!")
